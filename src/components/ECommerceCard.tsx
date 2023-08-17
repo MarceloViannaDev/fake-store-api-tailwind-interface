84cc1962361121c3e9f7e875aa981df879cd6e72
Marcelo Vianna Pacheco <marceloviannadev@gmail.com>
2023-08-17 19:50:11 -0300

    Code review in some parts

--- src/components/ECommerceCard.tsx
+++ src/components/ECommerceCard.tsx
@@ -17,42 +17,34 @@ export default function ECommerceCard() {
   useEffect(() => {
     axios
       .get<Product[]>('https://fakestoreapi.com/products')
-      .then((response) => {
-        setProducts(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching products:', error);
-      });
+      .then((response) => setProducts(response.data))
+      .catch(console.error);
   }, []);
-
-  const shouldWrap = products.length > 1;
 
   return (
     <div
-      className={`gap-8 w-96 h-auto ${shouldWrap ? 'flex-row flex-wrap' : ''}`}>
-      {products.map((product) => (
+      className={`gap-8 w-96 h-auto ${
+        products.length > 1 ? 'flex-row flex-wrap' : ''
+      }`}>
+      {products.map(({ id, title, image, rating, price }) => (
         <div
-          key={product.id}
+          key={id}
           className="bg-gray-900 shadow-lg rounded-lg overflow-hidden mb-4">
-          <img
-            className="w-full h-48 object-cover"
-            alt={product.title}
-            src={product.image}
-          />
+          <img className="w-full h-48 object-cover" alt={title} src={image} />
           <div className="p-4">
             <a
               href="#"
               className="text-xl font-semibold text-gray-900 dark:text-white">
-              {product.title}
+              {title}
             </a>
             <div className="mb-2.5 flex items-center">
               <span className="ml-3 mr-2 rounded bg-cyan-100 px-2.5 py-0.5 text-xs font-semibold text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800">
-                {product.rating.rate}
+                {rating.rate}
               </span>
             </div>
             <div className="flex items-center justify-between">
               <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                ${product.price}
+                ${price}
               </span>
               <a
                 className="rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
@@ -66,3 +58,8 @@ export default function ECommerceCard() {
     </div>
   );
 }
+
+/* The changes made are:
+
+1. Removed unnecessary console log message in catch block of axios call.
+2. Used destructuring in map function to make code more readable. */
